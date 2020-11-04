337df9236334b3dd192a26532cbe1606902b809b
tyy <1395935398@qq.com>
2020-11-04 21:19:05 +0800

    add

--- Numerical/GPU/test.jl
+++ Numerical/GPU/test.jl
@@ -2,13 +2,13 @@ using CUDA, Interpolations
 # N is the dimensionality (1, 2 or 3)
 # T is the element type (needs to be supported by the texture hardware)
 device!(1)
-T=Float32
+T=Float64
 N=1
 # source array
 src = rand(T, fill(10, N)...)
 
 # indices we want to interpolate
-idx = [tuple(rand(1f0:0.1f0:10f0, N)...) for _ in 1:10]
+idx = [tuple(rand(1:0.1:10, N)...) for _ in 1:10]
 
 # upload to the GPU
 gpu_src = CuArray(src)
@@ -16,13 +16,12 @@ gpu_idx = CuArray(idx)
 
 # interpolate using a texture
 gpu_dst = CuArray{T}(undef, size(gpu_idx))
-gpu_tex = CuTexture(gpu_src; interpolation=CUDA.NearestNeighbour())
+gpu_tex = CuTexture(gpu_src, interpolation=CUDA.NearestNeighbour())
 
-a=similar(gpu_dst)
-broadcast!(a,a,gpu_dst, gpu_idx, Ref(gpu_tex))
+gpu_tex[1]
 
-broadcast!(gpu_dst, gpu_idx, Ref(gpu_tex)) do midx, tex
-    tex[midx...]
+broadcast!(gpu_dst, gpu_idx, Ref(gpu_tex)) do idx, tex
+    tex[idx...]
 end
 
 # back to the CPU
